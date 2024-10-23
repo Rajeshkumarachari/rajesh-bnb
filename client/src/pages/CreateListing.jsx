b736a93112ee533d044d26d2ac22b5419ef92fa5
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-10-23 22:26:15 +0530

    complete upload listing images functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,10 +1,88 @@
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { useState } from "react";
 import { IoCloudUploadOutline } from "react-icons/io5";
+import { PiListChecksFill } from "react-icons/pi";
+import { TiDeleteOutline } from "react-icons/ti";
+import { MdErrorOutline } from "react-icons/md";
+import { app } from "../firebase";
 export default function CreateListing() {
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    imageUrls: [],
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+
+  const handleImageSubmit = () => {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          setImageUploadError(
+            "Image upload failed (2 mb max per image)" + err.message
+          );
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError("You can only upload 6 images per listing");
+      setUploading(false);
+    }
+  };
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      imageUrls: formData.imageUrls.filter((_, i) => i !== index),
+    });
+  };
   return (
     <main className=" p-3 mt-5 rounded-lg shadow-sm max-w-4xl mx-auto bg-slate-50">
-      <h1 className=" text-2xl font-medium text-center my-5">
+      <h1 className="flex items-center hover:bg-slate-200 justify-center gap-2 text-2xl font-medium text-center my-5">
+        <PiListChecksFill className=" size-8 text-rose-500" />
         Create a Listing
       </h1>
+      <hr className=" my-5 w-[25%] mx-auto border-gray-300 " />
       <form className=" flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1  ">
           <input
@@ -37,7 +115,7 @@ export default function CreateListing() {
             </div>
             <div className="flex gap-2">
               <input type="checkbox" id="rent" className="w-5" />
-              <span>rent</span>
+              <span>Rent</span>
             </div>
             <div className="flex gap-2">
               <input type="checkbox" id="parking" className="w-5" />
@@ -107,26 +185,60 @@ export default function CreateListing() {
         </div>
         <div className="flex flex-col flex-1 gap-4">
           <p className=" font-semibold">
-            Images:{" "}
+            Images:
             <span className=" font-normal text-gray-700 ml-2">
-              {" "}
               The first image will be the cover (max 6)
             </span>
           </p>
           <div className="flex gap-4">
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className=" p-3 border border-gray-300 rounded w-full"
               type="file"
               id="images"
               accept="image/*"
               multiple
             />
-            <button className="flex items-center gap-2 p-3 text-green-700 border border-green-700 rounded hover:shadow-lg disabled:opacity-80">
+            <button
+              disabled={uploading}
+              type="button"
+              onClick={handleImageSubmit}
+              className="flex items-center gap-2 p-3 text-green-700 border border-green-700 rounded hover:shadow-lg disabled:opacity-80"
+            >
               <IoCloudUploadOutline className=" size-6" />
-              Upload
+              {uploading ? "Uploading..." : "Upload"}
             </button>
           </div>
-          <button className=" mt-4 p-3 bg-slate-700 text-white rounded-lg  hover:opacity-95 ">
+
+          <p className="text-orange-700 flex text-sm ">
+            {imageUploadError && (
+              <p className=" flex items-center gap-2">
+                <MdErrorOutline className=" size-5" /> {imageUploadError}
+              </p>
+            )}
+          </p>
+          {formData.imageUrls.length > 0 &&
+            formData.imageUrls.map((url, index) => (
+              <div
+                key={url}
+                className="flex justify-between p-3 border items-center"
+              >
+                <img
+                  src={url}
+                  alt="listing image"
+                  className="w-20 h-20 object-contain rounded-lg"
+                />
+                <button
+                  type="button"
+                  onClick={() => handleRemoveImage(index)}
+                  className="flex items-center gap-2 p-3 hover:bg-red-50 border text-red-700 rounded-lg text-base hover:opacity-75"
+                >
+                  <TiDeleteOutline className=" size-6" />
+                  Delete
+                </button>
+              </div>
+            ))}
+          <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
             Create Listing
           </button>
         </div>
