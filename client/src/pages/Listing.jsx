57103eb1a4dee35764afbbaa56eb6e53724412cf
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-10-25 07:41:33 +0530

    add image slider to the listing page

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,131 @@
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import SwiperCore from "swiper";
+// import { Navigation } from "swiper/modules";
+// import "swiper/css/bundle";
+
+// export default function Listing() {
+//   SwiperCore.use[Navigation];
+//   const params = useParams();
+//   const [listing, setListing] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
+//   useEffect(() => {
+//     const fetchListing = async () => {
+//       try {
+//         setLoading(true);
+//         const res = await fetch(`/api/listing/get/${params.listingId}`);
+//         const data = await res.json();
+//         if (data.success === false) {
+//           setError(true);
+//           setLoading(false);
+//           return;
+//         }
+//         setListing(data);
+//         setLoading(false);
+//         setError(false);
+//       } catch (error) {
+//         console.log(error);
+//         setError(true);
+//         setLoading(false);
+//       }
+//     };
+//     fetchListing();
+//   }, [params.listingId]);
+//   console.log(loading);
+//   return (
+//     <main className=" bg-white">
+//       {loading && (
+//         <p className=" text-center mt-5 pt-6 text-rose-700  flex justify-center text-2xl gap-2">
+//           Loading...
+//         </p>
+//       )}
+//       {error && (
+//         <p className="text-center my-7 text-2xl">Something went wrong!</p>
+//       )}
+//       {listing && !loading && !error && (
+//         <div>
+//           <Swiper navigation>
+//             {listing.imageUrls.map((url) => (
+//               <SwiperSlide key={url}>
+//                 <div
+//                   className="h-[550px]"
+//                   style={{
+//                     background: `url(${url}) center no-repeat`,
+//                     backgroundSize: "cover",
+//                   }}
+//                 ></div>
+//               </SwiperSlide>
+//             ))}
+//           </Swiper>
+//         </div>
+//       )}
+//     </main>
+//   );
+// }
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+export default function Listing() {
+  SwiperCore.use([Navigation]);
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const params = useParams();
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setListing(data);
+        setLoading(false);
+        setError(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
+  console.log(loading);
+  return (
+    <main>
+      {loading && (
+        <p className="text-center my-7 text-2xl">
+          Loading...
+          <span className="loading loading-spinner text-error "></span>
+        </p>
+      )}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong!</p>
+      )}
+      {listing && !loading && !error && (
+        <div>
+          <Swiper navigation>
+            {listing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[550px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </main>
+  );
+}
